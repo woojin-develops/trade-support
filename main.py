bce1150e3a3108cd768094dc7369a8d868d1827e
Lumiaire <99623262+Lumiaire@users.noreply.github.com>
2022-12-29 11:04:03 -0800

    Update main.py
    added functionality to poll()

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import requests
-from threading import Thread
+import colorama
+import datetime
+import time
+from bs4 import BeautifulSoup
+from colorama import Fore
+from requests import Session
 
+
+colorama.init(autoreset=True)
 with open('proxies.txt', 'r+', encoding='utf-8') as f:
-    networks = f.read().splitlines()
+    proxies = f.read().splitlines() #useless atm
 
-def getNetwork(networks):
-    for network in networks:
-        res = requests.get(network).text
-        yield res
+def getNetwork(URL):
+    time.sleep(1)
+    res = requests.get(URL).text
+    soup = BeautifulSoup(res, 'lxml')
+    price = soup.find('div', {'class':'YMlKec fxKbKc'}).text
+    return soup, price
 
 class Polling:
-    def __init__(self):
-        self.tickers = []
-
-    def poll(self, symbol):
-        pass
+    def __init__(self, symbols, exchange = 'nasdaq'):
+        symbols = [symbol.strip(' ') for symbol in symbols.split(',')]
+        self.tickers = [f'https://www.google.com/finance/quote/{symbol}:{exchange}?hl=en' for symbol in symbols] 
+    
+    def poll(self):
+        for URL in self.tickers:
+            soup, price = getNetwork(URL)
+            ticker = URL.split('/')[5].split(':')[0]
+            if (previousPrice:=soup.find('div', {'class':'P6K39c'}).text) > price:
+                print(f'{datetime.datetime.now()}{Fore.RED}{ticker} {Fore.GREEN}{previousPrice} -----> {Fore.RED}{price}')
+            else:
+                print(f'{datetime.datetime.now()}{Fore.GREEN}{ticker} {Fore.RED}{previousPrice} -----> {Fore.GREEN}{price}')
+            stockQuotes = soup.find_all('div', {'class':'yY3Lee'})
+            for quote in stockQuotes:
+                try:
+                    relevancy = quote.find('div', {'class':'Yfwt5'}).text
+                    caption = quote.find('div', {'class':'Adak'}).text
+                    source = quote.find('div', {'class':'sfyJob'}).text
+                    sourceLink = quote.find('a').get('href')
+                except AttributeError:
+                    print('AttributeError: NoneType object has no attribute text')
+                else:
+                    print(f'{caption}:{source}:{relevancy}\n{sourceLink}')
