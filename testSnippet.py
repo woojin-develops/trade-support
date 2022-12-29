53fc083a2f8522a2946b8a99a97ede94d51f1f88
Lumiaire <99623262+Lumiaire@users.noreply.github.com>
2022-12-29 11:04:24 -0800

    Delete testSnippet.py

--- testSnippet.py
+++
@@ -1,9 +0,0 @@
-import requests
-import re
-from bs4 import BeautifulSoup   
-res = requests.get("https://www.bing.com/search?q=%tsla").text
-soup = BeautifulSoup(res, "lxml")
-scrapes = soup.find("div", {"id":"b_content"}, {})
-spec = scrapes.fetchPreviousSiblings
-print(scrapes)
-
