56bedd47896544fc0bc8fd69524b92a991a5b34e
Lumiaire <99623262+Lumiaire@users.noreply.github.com>
2022-12-31 11:04:49 -0800

    Update main.py
    +readability
    +edge cases
    +analzyze()

--- main.py
+++ main.py
@@ -2,46 +2,56 @@ import requests
 import colorama
 import datetime
 import time
-from bs4 import BeautifulSoup
-from colorama import Fore
-from requests import Session
-from itertools import cycle
 import nltk
+from bs4 import BeautifulSoup
+from itertools import cycle
+from collections import defaultdict
+from colorama import Fore
 
 colorama.init(autoreset=True)
 with open('proxies.txt', 'r+', encoding='utf-8') as f:
-    ProxyPool = cycle(f.read().splitlines()) #useless atm
+    proxypool = cycle(f.read().splitlines()) #useless atm
 
 def getNetwork(URL):
     time.sleep(1)
-    res = requests.get(URL,).text
+    res = requests.get(URL).text
     soup = BeautifulSoup(res, 'lxml')
-    price = soup.find('div', {'class':'YMlKec fxKbKc'}).text
+    try:
+        price = soup.find('div', {'class':'YMlKec fxKbKc'}).text
+    except AttributeError:
+        price = None
     return soup, price
 
 class Polling:
     def __init__(self, symbols, exchange = 'nasdaq'):
-        symbols = [symbol.strip(' ') for symbol in symbols.split(',')]
-        self.tickers = [f'https://www.google.com/finance/quote/{symbol}:{exchange}?hl=en' for symbol in symbols] 
-    
+        self.symbols = [symbol.strip(' ') for symbol in symbols.split(',')]
+        self.tickers = [f'https://www.google.com/finance/quote/{symbol}:{exchange}?hl=en' for symbol in self.symbols] 
+        self.sources = defaultdict(list)
     def poll(self):
         for URL in self.tickers:
             soup, price = getNetwork(URL)
             ticker = URL.split('/')[5].split(':')[0]
-            if (previousPrice:=soup.find('div', {'class':'P6K39c'}).text) > price:
-                print(f'{Fore.BLUE}{datetime.datetime.now()} {Fore.RED}{ticker} {Fore.GREEN}{previousPrice} -----> {Fore.RED}{price}')
+            stock_quotes = soup.find_all('div', {'class':'yY3Lee'})
+            if (previous_price := soup.find('div', {'class':'P6K39c'}).text) > price:
+                print(f'{Fore.BLUE}{datetime.datetime.now()} {Fore.RED}{ticker} {Fore.GREEN}{previous_price} -----> {Fore.RED}{price}')
             else:
-                print(f'{Fore.BLUE}{datetime.datetime.now()} {Fore.GREEN}{ticker} {Fore.RED}{previousPrice} -----> {Fore.GREEN}{price}')
-            stockQuotes = soup.find_all('div', {'class':'yY3Lee'})
-            for quote in stockQuotes:
+                print(f'{Fore.BLUE}{datetime.datetime.now()} {Fore.GREEN}{ticker} {Fore.RED}{previous_price} -----> {Fore.GREEN}{price}')
+            for quote in stock_quotes:
                 try:
                     relevancy = quote.find('div', {'class':'Yfwt5'}).text
                     caption = quote.find('div', {'class':'Adak'}).text
                     source = quote.find('div', {'class':'sfyJob'}).text
-                    sourceLink = quote.find('a').get('href')
+                    source_link = quote.find('a').get('href')
                 except AttributeError:
                     print('AttributeError: NoneType object has no attribute text')
                 else:
-                    print(f'{caption}:{source}:{relevancy}\n{sourceLink}')
-BEST = Polling('aapl, msft, tlry, tsla')
-BEST.poll()
+                    self.sources[f'{ticker}'].append(source_link)
+                    print(f'{caption}:{source}:{relevancy}\n{source_link}')
+    def analyze(self):
+        soup, _ = getNetwork(self.sources['tlry'][0])
+        main_text = soup.find('body').text
+        tokens = nltk.word_tokenize(main_text)
+        print(tokens)
+test = Polling('tlry, aapl, msft')
+test.poll()
+test.analyze()
